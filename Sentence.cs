f92963926ab221e325568f0b31a33f0b60b8738d
Dmitri <56200546+Novurix@users.noreply.github.com>
2020-06-07 15:13:18 -0400

    template for a sentence

---
+++ Sentence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace SearchingSystem
+{
+    public class Sentence
+    {
+        public string sentence = "";
+        public float accuracy = 0;
+
+        public bool hasBeenListed = false;
+
+        public Sentence(string sentence)
+        {
+            this.sentence = sentence;
+        }
+
+        public void increaseAccuracy(float accuracy)
+        {
+            this.accuracy += accuracy;
+            //Console.WriteLine(sentence + " " + this.accuracy);
+        }
+    }
+}
